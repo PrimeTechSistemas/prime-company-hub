52d15999689bacb6649278eab22164410df2c7d7
ArthenyoCarlos <teteno.acgba@gmail.com>
2026-06-08 16:00:39 -0300

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/br/com/primetechsistema/companyhub/PrimeCompanyHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.primetechsistema.companyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeCompanyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeCompanyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/primetechsistema/companyhub/PrimeCompanyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.primetechsistema.companyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeCompanyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
